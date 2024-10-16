85292f437b6a342f6b2a198f62919e1ea25f7d7d
Ismael Garcia <leamsigc@hotmail.com>
2024-10-17 00:04:19 +0200

    Update json and script to create folder for components

--- scripts/create-crud.mjs
+++ scripts/create-crud.mjs
@@ -1,7 +1,6 @@
 import { mkdir, readFile, writeFile } from "fs/promises";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
-import { camelCase, kebabCase, pascalCase } from "nuxt/dist/utils/string";
 
 const currentDirname = dirname(fileURLToPath(import.meta.url));
 const rootDir = join(currentDirname, "../");
@@ -13,12 +12,33 @@ if (!crudName) {
   throw new Error("Please specify a crudName.");
 }
 
+function kebabCase(str) {
+  return str
+    .toLowerCase()
+    .replace(/\s+/g, "-") // replace one or more spaces with a single dash
+    .replace(/[^\w-]+/g, ""); // remove any non-word characters (except dashes)
+}
+
+function camelCase(str) {
+  return str
+    .toLowerCase()
+    .replace(/\s+|[-_]+/g, "") // replace spaces, dashes, or underscores with a single space
+    .split(" ")
+    .map((word, index) => (index === 0 ? word : word.charAt(0).toUpperCase() + word.slice(1)))
+    .join("");
+}
+
+function pascalCase(str) {
+  return camelCase(str)
+    .charAt(0)
+    .toUpperCase() + camelCase(str).slice(1);
+}
 const crudNameKebab = kebabCase(crudName);
 const crudNameCamel = camelCase(crudName);
 const crudNamePascal = pascalCase(crudName);
 
 // UI related templates
-const componentsDir = join(rootDir, "./app/components");
+const componentsDir = join(rootDir, `./app/components/${crudNamePascal}`);
 const usersTableFile = join(componentsDir, `${crudNamePascal}Table.vue`);
 const userCreateFile = join(componentsDir, `${crudNamePascal}Create.vue`);
 const userUpdateFile = join(componentsDir, `${crudNamePascal}Update.vue`);
